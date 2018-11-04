81e9905bfc84de5c015e0aa3359dd12dd783d9e3
Chandra Sai <chandrasai_b150744cs@nitc.ac.in>
2018-11-04 06:27:12 +0530

    faculty completed 1.1

--- app/src/main/java/com/dc/chandra/nitchatzz/MainActivity.java
+++ app/src/main/java/com/dc/chandra/nitchatzz/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if(toolbar != null) {
             setSupportActionBar(toolbar);
-            getSupportActionBar().setTitle("NITChatzz");
+            getSupportActionBar().setTitle("NITChatzz FA");
         }
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         floatButton = (FloatingActionButton) findViewById(R.id.fab);
@@ -200,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.about) {
-            Toast.makeText(this, "Rivchat version 1.0", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "NITChatzz version 1.0", Toast.LENGTH_LONG).show();
             return true;
         }
 
